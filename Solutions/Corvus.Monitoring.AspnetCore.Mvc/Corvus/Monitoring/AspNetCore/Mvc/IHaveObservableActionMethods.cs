6be25fa90c80d5f37facaa74c7e7ef405d53b179
Jonathan George <github@jonathangeorge.co.uk>
2023-08-08 11:47:57 +0100

    Add new Corvus.Monitoring.AspnetCore.Mvc package

---
+++ Solutions/Corvus.Monitoring.AspnetCore.Mvc/Corvus/Monitoring/AspNetCore/Mvc/IHaveObservableActionMethods.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="IHaveObservableActionMethods.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+namespace Corvus.Monitoring.AspnetCore.Mvc
+{
+    using Corvus.Monitoring.Instrumentation;
+
+    /// <summary>
+    /// Interface for controller methods which are decorated with the <see cref="ObservableActionMethodsAttribute"/>
+    /// to assist with adding observation data during action method execution.
+    /// </summary>
+    public interface IHaveObservableActionMethods
+    {
+        /// <summary>
+        /// Gets or sets the <see cref="IOperationInstance"/> for execution of the current action method. This property
+        /// will be set prior to action method execution and unset afterwards.
+        /// </summary>
+        IOperationInstance? CurrentOperation { get; set; }
+    }
+}
---
+++ Solutions/Corvus.Monitoring.AspnetCore.Mvc/Corvus/Monitoring/AspNetCore/Mvc/ObservableActionMethodsAttribute.cs
@@ -0,0 +1,54 @@
+﻿// <copyright file="ObservableActionMethodsAttribute.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+namespace Corvus.Monitoring.AspnetCore.Mvc
+{
+    using System.Threading.Tasks;
+    using Corvus.Monitoring.Instrumentation;
+    using Microsoft.AspNetCore.Mvc.Controllers;
+    using Microsoft.AspNetCore.Mvc.Filters;
+    using Microsoft.Extensions.DependencyInjection;
+
+    /// <summary>
+    /// Action filter which logs execution times for action methods.
+    /// </summary>
+    public class ObservableActionMethodsAttribute : ActionFilterAttribute
+    {
+        /// <inheritdoc/>
+        public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            IOperationsInstrumentation instrumentation = context.HttpContext.RequestServices.GetRequiredService<IOperationsInstrumentation>();
+
+            // ActionFilters can only be applied to Controllers, so we know that the ActionDescriptor we have
+            // will be a ControllerActionDescriptor and can use a cast expression.
+            var actionDescriptor = (ControllerActionDescriptor)context.ActionDescriptor;
+
+            using IOperationInstance operation = instrumentation.StartOperation($"{actionDescriptor.ControllerTypeInfo.FullName}.{actionDescriptor.MethodInfo.Name}");
+
+            // Add route parameters to the current operation data
+            operation.AddRouteData(context.RouteData);
+
+            if (context.Controller is IHaveObservableActionMethods observableController)
+            {
+                observableController.CurrentOperation = operation;
+            }
+
+            await next.Invoke();
+        }
+
+        /// <inheritdoc/>
+        public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            IOperationsInstrumentation instrumentation = context.HttpContext.RequestServices.GetRequiredService<IOperationsInstrumentation>();
+
+            // ActionFilters can only be applied to Controllers, so we know that the ActionDescriptor we have
+            // will be a ControllerActionDescriptor and can use a cast expression.
+            var actionDescriptor = (ControllerActionDescriptor)context.ActionDescriptor;
+
+            using IOperationInstance operation = instrumentation.StartOperation($"{actionDescriptor.ControllerTypeInfo.FullName}.{actionDescriptor.MethodInfo.Name}::ResultExecution");
+
+            await next.Invoke();
+        }
+    }
+}
